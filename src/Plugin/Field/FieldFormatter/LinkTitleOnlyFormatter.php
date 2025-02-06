719d0c3d947aca86b122c7106e49e636406cbf44
Edouard Cunibil <edouard@happyculture.coop>
2025-02-06 01:05:15 +0100

    Fix LinkTitleOnlyFormatter namespace.

--- src/Plugin/Field/FieldFormatter/LinkTitleOnlyFormatter.php
+++ src/Plugin/Field/FieldFormatter/LinkTitleOnlyFormatter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\ccu_core\Plugin\Field\FieldFormatter;
+namespace Drupal\kumquat_core\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
